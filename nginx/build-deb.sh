c4a9e85ee4272f39c621ff798b65ea423b363b1f
VirtuBox <contact@virtubox.net>
2017-12-07 20:52:43 +0100

    Update build-deb.sh

--- nginx/build-deb.sh
+++ nginx/build-deb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt-get install -y build-essential libtool automake autoconf zlib1g-dev libpcre3-dev libssl-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
+
+apt-get install -y build-essential libtool automake autoconf zlib1g-dev libpcre3-dev libgd-dev libssl-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev
+
+rm -rf /usr/local/src/*
 cd /usr/local/src
-wget http://nginx.org/download/nginx-1.13.7.tar.gz
-tar -xzvf nginx-1.13.7.tar.gz
-mv nginx-1.13.7 nginx
 
 git clone https://github.com/FRiCKLE/ngx_cache_purge.git
 git clone https://github.com/openresty/memc-nginx-module.git
@@ -16,6 +16,7 @@ git clone https://github.com/openresty/srcache-nginx-module.git
 git clone https://github.com/openresty/set-misc-nginx-module.git
 git clone https://github.com/FRiCKLE/ngx_coolkit.git
 git clone https://github.com/FRiCKLE/ngx_slowfs_cache.git
+
 wget https://people.freebsd.org/~osa/ngx_http_redis-0.3.8.tar.gz
 tar -zxf ngx_http_redis-0.3.8.tar.gz
 mv ngx_http_redis-0.3.8 ngx_http_redis
@@ -23,15 +24,24 @@ mv ngx_http_redis-0.3.8 ngx_http_redis
 git clone https://github.com/google/ngx_brotli.git
 cd ngx_brotli
 git submodule update --init --recursive
+
 cd /usr/local/src
 
 git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout tls1.3-draft-18
 
+cd /usr/local/src/
+
+wget http://nginx.org/download/nginx-1.13.7.tar.gz
+tar -xzvf nginx-1.13.7.tar.gz
+mv nginx-1.13.7 nginx
+
 cd /usr/local/src/nginx/
+
 wget https://raw.githubusercontent.com/cujanovic/nginx-dynamic-tls-records-patch/master/nginx__dynamic_tls_records_1.11.5%2B.patch
 patch -p1 < nginx__dynamic_tls_records_1.11.5*.patch
+
 
 ./configure \
  --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2' \
